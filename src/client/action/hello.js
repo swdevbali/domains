2fd8d1e008d3359ccefed015f058a6b74aaad052
Eko Wibowo <eko@monetizemore.com>
2017-04-26 21:02:25 +0700

    react-redux, immutable, reducer

---
+++ src/client/action/hello.js
@@ -0,0 +1,4 @@
+import { createAction } from 'redux-actions'
+
+export const SAY_HELLO = 'SAY_HELLO'
+export const sayHello = createAction(SAY_HELLO)
---
+++ src/client/reducer/hello.js
@@ -0,0 +1,19 @@
+import Immutable from 'immutable'
+import type { fromJS as Immut} from 'immutable'
+
+import { SAY_HELLO } from '../action/hello'
+
+const initialState = Immutable.fromJS({
+  message: 'Initial message',
+})
+
+const helloReducer = (state: Immut = initialState, action: {type: string, payload: any }) => {
+  switch(action.type) {
+    case SAY_HELLO:
+      return state.set('message', action.payload)
+    default:
+      return state
+  }
+}
+
+export default helloReducer
